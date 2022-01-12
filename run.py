5ba4412f6dc0d3ffc329c6d36bd7e68790440dfd
joachimnorden <joachim.norden@gmail.com>
2022-01-12 20:29:03 +0000

    Add board variable and display_board function

--- run.py
+++ run.py
@@ -1,13 +1,24 @@
+board = ["-", "-", "-", 
+         "-", "-", "-",
+         "-", "-", "-"]
 
 
+class tic_tac_toe:
+
+    def display_board():
+        print(board[0] + " | " + board[1] + " | " + board[2])
+        print(board[3] + " | " + board[4] + " | " + board[5])
+        print(board[6] + " | " + board[7] + " | " + board[8])
+
+    display_board()
 
 # board
 # display board
 # play game
 # handle turn
 # check win
-    #check rows
-    #check columns
-    #check diagonals
-#check tie
-#flip player
+    # check rows
+    # check columns
+    # check diagonals
+# check tie
+# flip player
