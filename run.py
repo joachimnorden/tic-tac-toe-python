da695e1731fb5fbc32e729c885147a9ebdbc4878
joachimnorden <joachim.norden@gmail.com>
2022-01-12 19:43:34 +0000

    Add functions to create as comments

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+# board
+# display board
+# play game
+# handle turn
+# check win
+    #check rows
+    #check columns
+    #check diagonals
+#check tie
+#flip player
