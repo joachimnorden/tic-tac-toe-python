fd1619d66ac7bee4e1766d4df2b1c84b5b9547f6
joachimnorden <joachim.norden@gmail.com>
2022-01-13 17:32:02 +0000

    Add initial functions

--- run.py
+++ run.py
@@ -3,14 +3,44 @@ board = ["-", "-", "-",
          "-", "-", "-"]
 
 
-class tic_tac_toe:
-
-    def display_board():
-        print(board[0] + " | " + board[1] + " | " + board[2])
-        print(board[3] + " | " + board[4] + " | " + board[5])
-        print(board[6] + " | " + board[7] + " | " + board[8])
-
+def start_game():
+    """
+    Play a game of Tic Tac Toe
+    """
     display_board()
+
+
+def display_board():
+    """
+    Display the board to screen
+    """
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+
+def handle_turn():
+
+
+def check_win():
+
+
+def check_rows():
+    
+
+def check_cols():
+
+
+def check_diagonal():
+
+
+def check_tie():
+
+
+def flip_player():    
+
+
+start_game()       
 
 # board
 # display board
