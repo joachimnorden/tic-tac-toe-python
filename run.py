78cf8a525d53282c720e1b90a871a89ffc773053
joachimnorden <joachim.norden@gmail.com>
2022-01-13 21:22:44 +0000

    Add check_win function from askpython.com

--- run.py
+++ run.py
@@ -23,18 +23,39 @@ def check_space(position):
         return False
 
 
+def check_win(game_piece, position):
+    # All possible winning combinations
+    soln = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
+            [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+ 
+    # Loop to check if any winning combination is satisfied
+    for x in soln:
+        if all(y in position[game_piece] for y in x):
+ 
+            # Return True if any winning combination satisfies
+            return True
+    # Return False if no combination is satisfied       
+    return False       
+ 
+
+# Function to check if the game is drawn
+def check_draw(position):
+    if len(position['X']) + len(position['O']) == 9:
+        return True
+    return False 
+
+
 def insert_game_piece(game_piece, position):
     """
     If the space is free add the game piece
     and check if any player has won.
     If not return another input.
     """
-    
     if check_space(position):
         board[position] = game_piece
         display_board(board)
 
-        if check_win():
+        if check_win(game_piece, position):
             if game_piece == "X":
                 print("Player 1 wins!")
                 exit()
@@ -47,10 +68,6 @@ def insert_game_piece(game_piece, position):
         position = int(input("Enter new position (1-9): "))
         insert_game_piece(game_piece, position)
         
-    return
-
-
-def check_win():
     return
 
 
@@ -77,4 +94,4 @@ def flip_player():
 
 
 insert_game_piece("x", 1)
-    
+insert_game_piece("x", 1)   
